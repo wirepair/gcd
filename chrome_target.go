8cdbe133ff44c7dd39cf3e94657e488869246229
wirepair <isaac.dawson@gmail.com>
2015-09-06 15:53:44 +0900

    fix websocket import to golang.org

--- chrome_target.go
+++ chrome_target.go
@@ -1,7 +1,7 @@
 /*
 The MIT License (MIT)
 
-Copyright (c) 2014 isaac dawson
+Copyright (c) 2015 isaac dawson
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
@@ -34,7 +34,7 @@ import (
 	"sync"
 	"sync/atomic"
 
-	"code.google.com/p/go.net/websocket"
+	"golang.org/x/net/websocket"
 )
 
 // default response object, contains the id and a result if applicable.
